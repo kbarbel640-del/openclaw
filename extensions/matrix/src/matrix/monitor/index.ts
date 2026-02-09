9447ea34fea4deaabed09012b55eb5647890a2fc
Monty Taylor <mordred@inaugust.com>
2026-02-09 06:55:48 -0700

    Matrix: add block streaming support for real-time message delivery

--- extensions/matrix/src/matrix/monitor/index.ts
+++ extensions/matrix/src/matrix/monitor/index.ts
@@ -285,6 +285,7 @@ export async function monitorMatrixProvider(opts: MonitorMatrixOpts = {}): Promi
     getRoomInfo,
     getMemberDisplayName,
     accountId: opts.accountId,
+    blockStreaming: accountConfig.blockStreaming,
   });
 
   registerMatrixMonitorEvents({
