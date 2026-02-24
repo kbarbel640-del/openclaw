khalf-code/aibot

fix(model-compat): add qwen thinkingFormat compat without lockfile churn

src/agents/model-compat.ts
@@ -6,8 +6,36 @@ function isOpenAiCompletionsModel(model: Model<Api>): model is Model<"openai-com
 
 export function normalizeModelCompat(model: Model<Api>): Model<Api> {
   const baseUrl = model.baseUrl ?? "";
-  const isZai = model.provider === "zai" || baseUrl.includes("api.z.ai");
-  if (!isZai || !isOpenAiCompletionsModel(model)) {
+  const providerKey = model.provider?.trim().toLowerCase() ?? "";
+
+  const isOpenAiCompletions = isOpenAiCompletionsModel(model);
+
+  // Qwen OpenAI-compatible endpoints (DashScope / Qwen Portal) use
+  // `enable_thinking` instead of `reasoning_effort`.
+  //
+  // pi-ai exposes this via `compat.thinkingFormat = "qwen"`.
+  const isQwenCompat =
+    isOpenAiCompletions &&
+    (providerKey === "qwen-portal" ||
+      providerKey === "dashscope" ||
+      baseUrl.includes("portal.qwen.ai") ||
+      baseUrl.includes("dashscope.aliyuncs.com"));
+  if (isQwenCompat) {
+    const openaiModel = model;
+    const compat = openaiModel.compat ?? undefined;
+
+    // Respect explicit thinkingFormat overrides.
+    if (!compat?.thinkingFormat) {
+      openaiModel.compat = compat
+        ? { ...compat, thinkingFormat: "qwen" }
+        : { thinkingFormat: "qwen" };
+    }
+    return openaiModel;
+  }
+
+  // z.ai OpenAI-compatible API does not support the OpenAI `developer` role.
+  const isZai = providerKey === "zai" || baseUrl.includes("api.z.ai");
+  if (!isZai || !isOpenAiCompletions) {
     return model;
   }
 

kbarbel640-del/openclaw

src/agents/model-compat.ts
@@ -14,13 +14,31 @@ function isDashScopeCompatibleEndpoint(baseUrl: string): boolean {
 
 export function normalizeModelCompat(model: Model<Api>): Model<Api> {
   const baseUrl = model.baseUrl ?? "";
-  const isZai = model.provider === "zai" || baseUrl.includes("api.z.ai");
+  const providerKey = model.provider?.trim().toLowerCase() ?? "";
+  const isOpenAiCompletions = isOpenAiCompletionsModel(model);
+
+  const isZai = providerKey === "zai" || baseUrl.includes("api.z.ai");
   const isMoonshot =
-    model.provider === "moonshot" ||
+    providerKey === "moonshot" ||
     baseUrl.includes("moonshot.ai") ||
     baseUrl.includes("moonshot.cn");
-  const isDashScope = model.provider === "dashscope" || isDashScopeCompatibleEndpoint(baseUrl);
-  if ((!isZai && !isMoonshot && !isDashScope) || !isOpenAiCompletionsModel(model)) {
+  const isDashScope = providerKey === "dashscope" || isDashScopeCompatibleEndpoint(baseUrl);
+
+  // Qwen OpenAI-compatible endpoints (DashScope / Qwen Portal) use
+  // `enable_thinking` instead of `reasoning_effort`.
+  //
+  // pi-ai exposes this via `compat.thinkingFormat = "qwen"`.
+  const isQwenCompat =
+    isOpenAiCompletions &&
+    (providerKey === "qwen-portal" || isDashScope || baseUrl.includes("portal.qwen.ai"));
+  if (isQwenCompat) {
+    const compat = model.compat ?? undefined;
+    if (!compat?.thinkingFormat) {
+      model.compat = compat ? { ...compat, thinkingFormat: "qwen" } : { thinkingFormat: "qwen" };
+    }
+  }
+
+  if (!isOpenAiCompletions || (!isZai && !isMoonshot && !isDashScope)) {
     return model;
   }
 