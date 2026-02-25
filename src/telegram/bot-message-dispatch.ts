daverjorge46/yoyoclaw

fix(telegram): remove ack reaction after block-streamed replies
When blockStreamingDefault is "on", replies are delivered as blocks
with no final reply queued (queuedFinal=false). The hasFinalResponse
check gated ack removal on queuedFinal||sentFallback, so the removal
was never reached despite deliveryState.delivered being true.

Include deliveryState.delivered in hasFinalResponse to ensure ack
reaction cleanup fires after block-streamed deliveries.

src/telegram/bot-message-dispatch.ts
@@ -327,7 +327,7 @@ export const dispatchTelegramMessage = async ({
     sentFallback = result.delivered;
   }
 
-  const hasFinalResponse = queuedFinal || sentFallback;
+  const hasFinalResponse = queuedFinal || sentFallback || deliveryState.delivered;
   if (!hasFinalResponse) {
     if (isGroup && historyKey) {
       clearHistoryEntriesIfEnabled({ historyMap: groupHistories, historyKey, limit: historyLimit });

kbarbel640-del/openclaw

src/telegram/bot-message-dispatch.ts
@@ -653,7 +653,7 @@ export const dispatchTelegramMessage = async ({
     sentFallback = result.delivered;
   }
 
-  const hasFinalResponse = queuedFinal || sentFallback;
+  const hasFinalResponse = queuedFinal || sentFallback || deliverySummary.delivered;
 
   if (statusReactionController && !hasFinalResponse) {
     void statusReactionController.setError().catch((err) => {